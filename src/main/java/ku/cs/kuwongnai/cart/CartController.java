9e6b0f6794c2cca0496d068a3587c799e032aefd
Qu1etboy <nonzagreanthai@gmail.com>
2023-09-26 11:50:45 +0700

    fix: get user id from jwt sub

--- src/main/java/ku/cs/kuwongnai/cart/CartController.java
+++ src/main/java/ku/cs/kuwongnai/cart/CartController.java
@@ -3,6 +3,8 @@ package ku.cs.kuwongnai.cart;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,18 +20,21 @@ public class CartController {
   private CartService cartService;
 
   @PostMapping
-  public void addToCart(@RequestParam String userId, @RequestBody CartItem cartItem) {
+  public void addToCart(@AuthenticationPrincipal Jwt jwt, @RequestBody CartItem cartItem) {
+    String userId = (String) jwt.getClaims().get("sub");
     cartService.addToCart(userId, cartItem);
-    System.out.println("Add to cart: " + cartItem);
+    System.out.println("Add to cart: " + cartItem + " From user id = " + userId);
   }
 
   @GetMapping
-  public List<CartItem> viewCart(@RequestParam String userId) {
+  public List<CartItem> viewCart(@AuthenticationPrincipal Jwt jwt) {
+    String userId = (String) jwt.getClaims().get("sub");
     return cartService.getCartItems(userId);
   }
 
   @DeleteMapping
-  public void removeFromCart(@RequestParam String userId, @RequestParam Long menuId) {
+  public void removeFromCart(@AuthenticationPrincipal Jwt jwt, @RequestParam Long menuId) {
+    String userId = (String) jwt.getClaims().get("sub");
     cartService.removeFromCart(userId, menuId);
   }
 
